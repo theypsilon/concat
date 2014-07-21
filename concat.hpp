1805cd2fda18c92ab546a86a5b254607397dab29
José manuel Barroso Galindo <theypsilon@gmail.com>
2014-07-21 21:06:57 +0700

    WS-1749 moving getRegionsAsListData to GenericAddressForm class

--- concat.hpp
+++ concat.hpp
@@ -2,16 +2,26 @@
 #define THEYPSILON_CONCAT
 
 #include <sstream>
+#include <deque>
 
 namespace theypsilon {
 
-    namespace separator {
-        constexpr char none [] = "";
-        constexpr char space[] = " ";
-        constexpr char endl [] = "\n";
-        constexpr char coma [] = ", ";
-        constexpr char plus [] = " + ";
-    }
+    struct separator {
+        const char* sep;
+        constexpr explicit separator(const char* s) noexcept: sep{s} {} 
+
+        const static char none [];
+        const static char space[];
+        const static char endl [];
+        const static char coma [];
+        const static char plus [];
+    };
+
+    const char separator::none [] = "";
+    const char separator::space[] = " ";
+    const char separator::endl [] = "\n";
+    const char separator::coma [] = ", ";
+    const char separator::plus [] = " + ";
 
     namespace { // is_container, is_stream
         template<typename T>
@@ -49,76 +59,210 @@ namespace theypsilon {
         struct is_container : std::integral_constant<bool, 
             has_const_iterator<T>::value && 
             has_begin_end<T>::beg_value  && 
-            has_begin_end<T>::end_value> {};
+            has_begin_end<T>::end_value  &&
+            !std::is_same<T, std::string>::value> {};
+
+        template<typename T> 
+        struct is_writable_stream : std::integral_constant<bool, 
+            std::is_same<T, std::ostringstream>::value ||
+            std::is_same<T, std::stringstream>::value> {};
 
         template<typename T> 
         struct is_stream : std::integral_constant<bool, 
             std::is_same<T, std::istringstream>::value ||
-            std::is_same<T, std::ostringstream>::value ||
-            std::is_same<T, std::stringstream>::value> {};
+            is_writable_stream<T>::value> {};
+
+        template<typename T> 
+        using char_sequence_type = typename std::remove_pointer<typename std::decay<T>::type>::type;
+
+        template<typename T> 
+        struct is_char_sequence : std::integral_constant<bool, 
+            std::is_same<char_sequence_type<T>, char    >::value || 
+            std::is_same<char_sequence_type<T>, wchar_t >::value || 
+            std::is_same<char_sequence_type<T>, char16_t>::value || 
+            std::is_same<char_sequence_type<T> ,char32_t>::value> {};
+
+        template<typename T> 
+        struct can_be_false : std::integral_constant<bool, 
+            is_stream<T>::value || 
+            is_char_sequence<T>::value> {};
+
+      template<typename T> 
+        struct is_basic_type : std::integral_constant<bool, 
+            std::is_scalar<T>::value ||
+            std::is_same<T,std::string>::value> {};
     }
 
     namespace { // concat_intern
-
-        template <typename W, typename S, typename T,
-        typename std::enable_if<is_container<T>::value == false && is_stream<T>::value == false, T>::type* = nullptr>
-        bool concat_intern_recursion(W& writter, const S& separator, const T& head) {
-            writter << head;
-            return true;
-        }
-
-        template <typename W, typename S>
-        bool concat_intern_recursion(W& writter, const S& separator, const char* head) {
-            if (!head) return false;
-            writter << head;
-            return true;
-        }
-
-        template <typename W, typename S, typename T, 
-        typename std::enable_if<is_stream<T>::value == true, T>::type* = nullptr>
-        bool concat_intern_recursion(W& writter, const S& separator, const T& head) {
-            if (!head) return false;
-            writter << head.str();
-            return true;
-        }
-
-        template <typename W, typename S, typename T,
-        typename std::enable_if<is_container<T>::value == true, T>::type* = nullptr>
-        bool concat_intern_recursion(W& writter, const S& separator, const T& seq) {
-            bool writting = false;
-            typename T::const_iterator it = std::begin(seq), et = std::end(seq);
-            while (it != et) {
-                bool b = concat_intern_recursion(writter, separator, *it);
-                ++it;
-                if (!b) continue;
-                writting = true;
-                if (it != et) writter << separator;
-            }
-            return writting;
-        }
-
-        template <typename W, typename S, typename T, typename... Args>
-        bool concat_intern_recursion(W& writter, const S& separator, const T& head, Args&&... tail) {
-            if (concat_intern_recursion(writter, separator, head)) 
-                writter << separator;
-            return concat_intern_recursion(writter, separator, tail...);
-        }
 
         template <typename W>
         std::string concat_to_string(const W& writter) {
             return writter ? writter.str() : "";
         }
 
-        template <typename S, typename... Args>
-        std::string concat_intern(const S& separator, std::stringstream& writter, const Args&... seq) {
-            concat_intern_recursion(writter, separator, seq...);
-            return concat_to_string(writter);
+        template <char head, char... tail>
+        std::string get_separator() { return {head, tail...}; }
+
+        namespace {
+
+            template <typename T,
+                typename std::enable_if<!can_be_false<T>::value && !is_container<T>::value, T>::type* = nullptr>
+            void must_write(std::deque<bool>& b, const T& v) {
+                b.push_back(std::is_scalar<T>::value ||
+                            std::is_same<T,std::string>::value);
+            }
+
+            template <typename T,
+                typename std::enable_if<can_be_false<T>::value && !is_container<T>::value, T>::type* = nullptr>
+            void must_write(std::deque<bool>& b, const T& v) {
+                b.push_back(((bool)v) == true);
+            }
+
+            template <typename T,
+                typename std::enable_if<!can_be_false<T>::value && is_container<T>::value, T>::type* = nullptr>
+            void must_write(std::deque<bool>& b, const T& container) {
+                for(const auto& element : container) {
+                    must_write(b, element);
+                    if (b.back())
+                        return b.push_back(true);
+                }
+                b.push_back(false);
+            }
+
+            template <typename T, typename... Args>
+            void must_write(std::deque<bool>& b, const T& head, const Args&... tail) {
+                must_write(b, head);
+                must_write(b, tail...);
+            }
+
+            template <typename... Args>
+            std::deque<bool> get_write_deque(const Args&... seq) {
+                std::deque<bool> b;
+                must_write(b, seq...);
+                return b;
+            }
+
+            bool any_writable(const std::deque<bool>& b) {
+                for (bool e: b) if (e) return true;
+                return false;
+            }
+
+            template <typename W, typename S, typename T>
+            void do_base_write(W& writter, const S& separator, std::deque<bool>& b, const T& v);
+
+            template <typename W, typename S, typename T,
+                typename std::enable_if<!is_container<T>::value && !can_be_false<T>::value && !is_basic_type<T>::value, T>::type* = nullptr>
+            void pre_write(W& writter, const S& separator, std::deque<bool>& b, const T& v) {
+                writter << v;
+            }
+
+            template <typename W, typename S, typename T,
+                typename std::enable_if<is_container<T>::value || can_be_false<T>::value || is_basic_type<T>::value, T>::type* = nullptr>
+            void pre_write(W& writter, const S& separator, std::deque<bool>& b, const T& v) {
+            }
+
+            template <typename W, typename S, typename T,
+                typename std::enable_if<!is_container<T>::value && !is_stream<T>::value, T>::type* = nullptr>
+            void do_write(W& writter, const S& separator, std::deque<bool>& b, const T& v) {
+                writter << v;
+            }
+
+            template <typename W, typename S, typename T,
+                typename std::enable_if<!is_container<T>::value && is_stream<T>::value, T>::type* = nullptr>
+            void do_write(W& writter, const S& separator, std::deque<bool>& b, const T& v) {
+                writter << v.str();
+            }
+
+            template <typename W, typename S, typename T,
+                typename std::enable_if<is_container<T>::value, T>::type* = nullptr>
+            void do_write(W& writter, const S& separator, std::deque<bool>&, const T& container) {
+                std::deque<bool> b;
+                for (const auto& element: container)
+                    must_write(b, element);
+                for (const auto& element: container)
+                    do_base_write(writter, separator, b, element);
+            }
+
+            template <typename W, typename S, typename T, typename... Args>
+            void do_write(W& writter, const S& separator, std::deque<bool>& b, const T& head, const Args&... tail) {
+                do_base_write(writter, separator, b, head);
+                do_write(writter, separator, b, tail...);
+            }
+
+            template <typename W, typename S, typename T>
+            inline void do_base_write(W& writter, const S& separator, std::deque<bool>& b, const T& v) {
+                pre_write(writter, separator, b, v);
+                bool must = b.front();
+                b.pop_front();
+                if (must && any_writable(b)) {
+                    do_write(writter, separator, b, v);
+                    writter << separator;        
+                }
+                
+            }
+
+            template <typename S, typename T, typename... Args,
+                typename std::enable_if<is_writable_stream<T>::value == true, T>::type* = nullptr>
+            std::string concat_intern(const S& separator, T& writter, const Args&... seq) {
+                auto b = get_write_deque(seq...);
+                do_write(writter, separator, b, seq...);
+                return concat_to_string(writter);
+            }
         }
 
-        template <typename S, typename... Args>
-        std::string concat_intern(const S& separator, std::ostringstream& writter, const Args&... seq) {
-            concat_intern_recursion(writter, separator, seq...);
-            return concat_to_string(writter);
+        namespace unused {
+
+            template <typename W, typename S, typename T,
+                typename std::enable_if<!is_container<T>::value && !is_stream<T>::value, T>::type* = nullptr>
+            bool concat_intern_recursion(W& writter, const S& separator, const T& head) {
+                writter << head;
+                return std::is_scalar<T>::value ||
+                       std::is_same<T,std::string>::value;
+            }
+
+            template <typename W, typename S>
+            bool concat_intern_recursion(W& writter, const S& separator, const char* head) {
+                if (!head) return false;
+                writter << head;
+                return true;
+            }
+
+            template <typename W, typename S, typename T, 
+                typename std::enable_if<is_stream<T>::value == true, T>::type* = nullptr>
+            bool concat_intern_recursion(W& writter, const S& separator, const T& head) {
+                if (!head) return false;
+                writter << head.str();
+                return true;
+            }
+
+            template <typename W, typename S, typename T,
+                typename std::enable_if<is_container<T>::value == true, T>::type* = nullptr>
+            bool concat_intern_recursion(W& writter, const S& separator, const T& seq) {
+                bool writting = false;
+                auto it = std::begin(seq), et = std::end(seq);
+                while (it != et) {
+                    bool b = concat_intern_recursion(writter, separator, *it);
+                    ++it;
+                    if (!b) continue;
+                    writting = true;
+                    if (it != et) writter << separator;
+                }
+                return writting;
+            }
+
+            template <typename W, typename S, typename T, typename... Args>
+            bool concat_intern_recursion(W& writter, const S& separator, const T& head, const Args&... tail) {            
+                if (concat_intern_recursion(writter, separator, head)) 
+                    writter << separator;
+                return concat_intern_recursion(writter, separator, tail...);
+            }
+
+            template <typename S, typename T, typename... Args,
+                typename std::enable_if<is_writable_stream<T>::value == true, T>::type* = nullptr>
+            std::string concat_intern(const S& separator, T& writter, const Args&... seq) {
+                concat_intern_recursion(writter, separator, seq...);
+                return concat_to_string(writter);
+            }
         }
 
         template <typename S, typename... Args>
@@ -128,19 +272,27 @@ namespace theypsilon {
         }
     }
 
-    template <char separator, typename... Args>
-    std::string concat(Args&&... seq) {
-        return concat_intern(separator, std::forward<Args>(seq)...);
+    template <typename... Args>
+    std::string concat(const separator& sep, Args&&... seq) {
+        return concat_intern(sep.sep, std::forward<Args>(seq)...);
     }
 
-    template <const char* separator = separator::none, typename... Args>
-    std::string concat(Args&&... seq) {
-        return concat_intern(separator, std::forward<Args>(seq)...);
+    template <char head, char... tail, typename F, typename... Args,
+        typename = typename std::enable_if<std::is_same<F, separator>::value == false, F>::type>
+    std::string concat(F&& first, Args&&... rest) {
+        return concat_intern(get_separator<head, tail...>(), std::forward<F>(first), std::forward<Args>(rest)...);
     }
 
-    template <std::ostream& (*separator) (std::ostream&), typename... Args>
-    std::string concat(Args&&... seq) {
-        return concat_intern(separator, std::forward<Args>(seq)...);
+    template <const char* sep = separator::none, typename F, typename... Args,
+        typename = typename std::enable_if<std::is_same<F, separator>::value == false, F>::type>
+    std::string concat(F&& first, Args&&... rest) {
+        return concat_intern(sep, std::forward<F>(first), std::forward<Args>(rest)...);
+    }
+
+    template <std::ostream& (*sep) (std::ostream&), typename F, typename... Args,
+        typename = typename std::enable_if<std::is_same<F, separator>::value == false, F>::type>
+    std::string concat(F&& first, Args&&... rest) {
+        return concat_intern(sep, std::forward<F>(first), std::forward<Args>(rest)...);
     }
 
 }
--- test.cpp
+++ test.cpp
@@ -1,15 +1,86 @@
-#include "concat.hpp"
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-
-#include <iostream>
+#include "concat.hpp"
 
 using namespace theypsilon;
 using namespace std;
 
-int main (int argc, char* argv[]) {
-	std::stringstream ss;
-	ss << "10, ";
-	int i = 3;
-	cout << concat<separator::coma>(1,2,3,4,5,6,7,8) << endl;
-	return 0;
+TEST_CASE( "Basic concat", "[concat]" ) {
+	std::string temp;
+
+	REQUIRE( concat(1,2,3,4,5)                  == "12345" );
+	REQUIRE( concat(separator(", "),1,2,3,4,5)  == "1, 2, 3, 4, 5" );
+	REQUIRE((concat<',', ' '>(1,2,3,4,5))       == "1, 2, 3, 4, 5" );
+	REQUIRE( concat<','>(1,2,3,4,5)             == "1,2,3,4,5" );
+	REQUIRE( concat<endl>(1,2,3,4,5)            == "1\n2\n3\n4\n5" );
+	REQUIRE( concat<separator::plus>(1,2,3,4,5) == "1 + 2 + 3 + 4 + 5" );
+	REQUIRE( concat(1,"2",3,"4",5,"6")          == "123456" );
+	REQUIRE( concat("1",2,"3",4,"5",6)          == "123456" );
+	REQUIRE( concat<' '>("Hello", "World!")     == "Hello World!" );
+	REQUIRE( concat('a',3,"b")                  == "a3b" );
+	REQUIRE( concat('a')              == "a");
+	REQUIRE( concat("a")              == "a");
+	REQUIRE( concat(std::string("a")) == "a");
+	REQUIRE( concat(true, false, true, false) == "1010");
+	REQUIRE( concat(static_cast<const char *>(nullptr)) == "");
+	REQUIRE( concat(static_cast<std::string*>(nullptr)) == "0");
+	REQUIRE( concat(static_cast<std::string*>(&temp)  ) != "0");
+	REQUIRE( concat(static_cast<const void *>(nullptr)) == "0");
+	REQUIRE( concat<' '>(std::setprecision(2), 4.0/3.0, 1, 2) == "1.3 1 2");
+
+	std::vector<int> v = {1,2,3,4,5};
+
+	REQUIRE( concat(v) == "12345" );
+	REQUIRE( concat(1,2,3,4,5,v) == "1234512345" );
+	REQUIRE( concat(v,1,2,3,4,5) == "1234512345" );
+	REQUIRE( concat(1,2,3,v,4,5) == "1231234545" );
+	REQUIRE( concat(v,v)         == "1234512345" );
+	REQUIRE( concat(v,1,v)       == "12345112345" );
+	REQUIRE( concat(1,v,v,1)     == "112345123451" );
+	REQUIRE( concat(1,v,1,v,1)   == "1123451123451" );
+	REQUIRE( concat(v,v,v)       == "123451234512345" );
+	REQUIRE( concat("something",v) == "something12345" );
+	REQUIRE( concat(v,"something") == "12345something" );
+
+	std::vector<std::string> s = {"hello"," ","world","!"};
+	REQUIRE( concat(s)      == "hello world!" );
+	REQUIRE( concat<' '>(s) == "hello   world !" );
+
+	std::vector<const char*> c = {"hello"," ","world","!"};
+	REQUIRE( concat(c)      == "hello world!" );
+	REQUIRE( concat<' '>(c) == "hello   world !" );
+
+	std::vector<char> ch = {'a','b','c'};
+	REQUIRE( concat(ch)      == "abc" );
+	REQUIRE( concat<' '>(ch) == "a b c" );
+
+	std::ostringstream s1, s2;
+	s1.exceptions(std::ifstream::failbit);
+	s1 << "hello";
+	s2 << "world!";
+	temp = concat<' '>(s1, s2);
+	REQUIRE( temp == s1.str() );
+	REQUIRE( temp == "helloworld!");
+	REQUIRE( concat(s1, 1,2,3, s1, s1) == "helloworld!123helloworld!123helloworld!123helloworld!123");
+
+	s1.str(""); s2.str("");	
+	s1.clear(); s2.clear();
+	s1 << "hello";
+	s2 << "world!";
+	temp = concat<' '>(static_cast<const std::ostringstream&>(s1), s2);
+	REQUIRE( temp     != s1.str() );
+	REQUIRE( temp     == "hello world!");
+	REQUIRE( s1.str() == "hello");
+
+	REQUIRE( concat(s1, s, c, ch, s2, "amazing") == "hellohello world!hello world!abcworld!amazing");
+	REQUIRE( s1.str()                            == "hellohello world!hello world!abcworld!amazing");
+
+	REQUIRE( concat(std::wstring()) == "");
+
+	const char* msg = nullptr;
+	REQUIRE( concat<' '>(msg) == "");
+	REQUIRE( concat<' '>("this is my message: ") == "this is my message: ");
+	REQUIRE( concat<' '>("this is my message: ", msg) == "this is my message: ");
+
+	REQUIRE( concat(u"This is a Unicode Character: \u2018.") == "" );
 }
